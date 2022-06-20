685c541e7312c9df91bf44f1f535946c555a4749
Duilio Fonseca-Gallardo <97610610+duiliofg@users.noreply.github.com>
2022-06-20 16:48:30 -0400

    Update get_dhdt.py
    update

--- get_dhdt.py
+++ get_dhdt.py
@@ -13,7 +13,7 @@ import scipy
 
 #Data lecture
 #Geopackage & Raster
-vec= gpd.read_file(r"/home/duilio/Desktop/mocho_50m_bandas/mocho_bandas_50m_porcuenca_32179.gpkg")
+vec= gpd.read_file(r"/home/duilio/Desktop/mocho_50m_bandas/mocho_bandas_50m_porcuenca_32719_3.gpkg")
 data= rs.open(r"/home/duilio/Desktop/mocho_50m_bandas/hugonnet_mocho_32719.tif")
 iden= [] # Id list
 poly= [] # Numpy matrix list
@@ -21,7 +21,7 @@ nmad_bb= [] # Normalized Median Absolute Deviation list
 area_pol=[] # Polygon's surface
 #Custom Attributes
 cod_gla= []#Lista de Codigos glaciar
-
+dhdt=[]
 #Raster Mask
 for i in range(vec["geometry"].shape[0]):    
     iout, _ = mask(data, vec.geometry[i], invert=False,crop=True)
@@ -41,14 +41,36 @@ for i in range(vec["geometry"].shape[0]):
     #Normalized Median Absolute Deviation (approximately mad(x)/0.6745)
     nmad=mad/scipy.stats.norm.ppf(3/4.)
     
-    #Data Filtering / Statistical treatment
-    values_mask[values_mask>mad*3]=np.nan
-    values_mask[values_mask<mad*(-3)]=np.nan
-    dhdt_pnd=(np.nanmean(values_mask)*area_pol/(vec["AREA_Km2"][i]*(10**6)))
-    print(dhdt_pnd)
-    
+    #Data Filtering
+    values_mask = values_mask[(values_mask <= nmad*3) & (values_mask >= nmad*(-3))]
+    #print(values_mask)   
+    #Clear 
+    dhdt_pnd=(np.mean(values_mask)*area_pol/(vec["AREA_Km2"][i]*(10**6)))
+    #print(dhdt_pnd)
     #Saving Vectors
     poly.append(iout)
     iden.append(vec["fid"][i])
     cod_gla.append(vec["COD_GLA"][i])
     nmad_bb.append(nmad)
+    dhdt.append(dhdt_pnd)
+
+# Add vector to geopackage field
+vec["dhdt_pnd"]=dhdt
+vec.to_file("dhdt_mocho_bandas50m.gpkg", driver="GPKG")
+
+#Results
+result_multicode=pd.DataFrame(data=cod_gla,index=iden,columns=['COD_GLA'])
+result_multicode.insert(1, "dhdt", dhdt, allow_duplicates=True)
+#List of glacier codes
+cod_gla_clean=np.unique(cod_gla)
+dhdt_per_glacod=[] #output
+
+# Mass balance per glacier
+for code in cod_gla_clean:
+    dhdt_gl=result_multicode.loc[result_multicode['COD_GLA'] == code]
+    dhdt_per_glacod.append(np.nansum(dhdt_gl['dhdt']))
+
+# Output
+output=pd.DataFrame(data=cod_gla_clean,index=cod_gla_clean,columns=['COD_GLA'])   
+output.insert(1, "dhdt", dhdt_per_glacod, allow_duplicates=True)
+output.to_csv('dhdt_hugonnet.csv', index=False, encoding='utf-8')
