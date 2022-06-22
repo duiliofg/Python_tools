1e04acac7bc4a2fe385093d69f0702c0962041f9
Duilio Fonseca-Gallardo <97610610+duiliofg@users.noreply.github.com>
2022-06-22 16:57:43 -0400

    Update get_dhdt.py
    update

--- get_dhdt.py
+++ get_dhdt.py
@@ -13,8 +13,8 @@ import scipy
 
 #Data lecture
 #Geopackage & Raster
-vec= gpd.read_file(r"/home/duilio/Desktop/mocho_50m_bandas/mocho_bandas_50m_porcuenca_32719_3.gpkg")
-data= rs.open(r"/home/duilio/Desktop/mocho_50m_bandas/hugonnet_mocho_32719.tif")
+vec= gpd.read_file(r"/home/duiliof/Desktop/BH/input/Inventario/Bandas/bandas_50m_18S_fid.gpkg")
+data= rs.open(r"/home/duiliof/Desktop/BH/BALANCE_GEODESICO/hugonnet/dhdt_hugonnet_chile_18s_32718_fix.tif")
 iden= [] # Id list
 poly= [] # Numpy matrix list
 nmad_bb= [] # Normalized Median Absolute Deviation list
@@ -23,7 +23,8 @@ area_pol=[] # Polygon's surface
 cod_gla= []#Lista de Codigos glaciar
 dhdt=[]
 #Raster Mask
-for i in range(vec["geometry"].shape[0]):    
+print("BLOCK 1: START")
+for i in range(vec["geometry"].shape[0]): 
     iout, _ = mask(data, vec.geometry[i], invert=False,crop=True)
     
     #Polygon Surface
@@ -45,7 +46,7 @@ for i in range(vec["geometry"].shape[0]):
     values_mask = values_mask[(values_mask <= nmad*3) & (values_mask >= nmad*(-3))]
     #print(values_mask)   
     #Clear 
-    dhdt_pnd=(np.mean(values_mask)*area_pol/(vec["AREA_Km2"][i]*(10**6)))
+    dhdt_pnd=(np.nanmean(values_mask)*area_pol/(vec["AREA_Km2"][i]*(10**6)))
     #print(dhdt_pnd)
     #Saving Vectors
     poly.append(iout)
@@ -53,11 +54,12 @@ for i in range(vec["geometry"].shape[0]):
     cod_gla.append(vec["COD_GLA"][i])
     nmad_bb.append(nmad)
     dhdt.append(dhdt_pnd)
-
+    clear_output(wait=True)
+    print(str(i)+" de "+str(range(vec["geometry"].shape[0])))          
 # Add vector to geopackage field
-vec["dhdt_pnd"]=dhdt
-vec.to_file("dhdt_mocho_bandas50m.gpkg", driver="GPKG")
-
+#vec["dhdt_pnd"]=dhdt
+#vec.to_file("dhdt_mocho_bandas50m.gpkg", driver="GPKG")
+print("BLOCK 1: DONE")
 #Results
 result_multicode=pd.DataFrame(data=cod_gla,index=iden,columns=['COD_GLA'])
 result_multicode.insert(1, "dhdt", dhdt, allow_duplicates=True)
@@ -69,8 +71,8 @@ dhdt_per_glacod=[] #output
 for code in cod_gla_clean:
     dhdt_gl=result_multicode.loc[result_multicode['COD_GLA'] == code]
     dhdt_per_glacod.append(np.nansum(dhdt_gl['dhdt']))
-
+print("BLOCK 2: DONE")    
 # Output
 output=pd.DataFrame(data=cod_gla_clean,index=cod_gla_clean,columns=['COD_GLA'])   
 output.insert(1, "dhdt", dhdt_per_glacod, allow_duplicates=True)
-output.to_csv('dhdt_hugonnet.csv', index=False, encoding='utf-8')
+output.to_csv('dhdt_18S.csv', index=False, encoding='utf-8')
